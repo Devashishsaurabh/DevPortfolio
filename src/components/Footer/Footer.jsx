0d2bf9cf29f2aac0165ec7a6db9719b842c899fa
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-07-02 14:32:00 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
       <div className="f-content">
         <span>onlyvishwasindia@gmail.com</span>
         <div className="f-icons">
-          <a href="https://github.com/Devashishsaurabh" target="_blank"><Linked color="#0077b5 " size={"3rem"} /></a>
+          <a href="https://github.com/Devashishsaurabh" target="_blank"><Gitub color="Black" size={"3rem"} /></a>
           <a href="https://www.instagram.com/devashish_saurabh_official" target="_blank"><Insta color="#cb3e7a" size={"3rem"} /></a>
-          <a href="https://www.linkedin.com/in/dev-ashish-saurabh/" target="_blank"><Gitub color="Black" size={"3rem"} /></a>
+          <a href="https://www.linkedin.com/in/dev-ashish-saurabh/" target="_blank"><Linked color="#0077b5 " size={"3rem"} /></a>
           <a href="https://medium.com/@onlyvishwasindia" target="_blank"><Medium color="white" size={"3rem"} /></a>
         </div>
       </div>
